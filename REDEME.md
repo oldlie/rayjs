e389730c71a7632d5334b88ba3c6e0438e819c7e
oldlie <chsq00@163.com>
2017-03-29 15:51:25 +0800

    Clear folders

--- REDEME.md
+++
@@ -1,3 +0,0 @@
-*** Ray JS 
-
-封装 adminlte 的常用组件
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-import { Core } from './core';
-
-import { ScoreProductApp } from './apps/score/product';
-
-let app = new ScoreProductApp('box', null);
-console.log(app.getId());
-app.run();
